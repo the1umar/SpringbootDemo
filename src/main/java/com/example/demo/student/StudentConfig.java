71091ff94c004aca7e5ee5c562313cf1a45f3426
Umar Afolami <afolamikoredel@gmail.com>
2026-01-26 16:16:59 -0600

    created student config to allow us save students to db (manually)

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,12 +1,36 @@
 package com.example.demo.student;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @Configuration
 public class StudentConfig {
 
-    CommandLineRunner commandLineRunner() {
-        return args -> {}
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student (
+                    "mariam.jamal@gmail.com",
+                    "Mariam",
+                    21,
+                    LocalDate.of(2000, Month.JANUARY, 5)
+            );
+
+            Student alex = new Student (
+                    "alex@gmail.com",
+                    "Alex",
+                    21,
+                    LocalDate.of(2004, Month.JANUARY, 5)
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
     }
 }
